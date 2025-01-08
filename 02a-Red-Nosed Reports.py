1702886d2c31a31d083c46aaa7935d0188d3b55f
Elutherius <lutheremail@duck.com>
2025-01-07 21:59:10 -0500

    day 2 improved

--- 02a-Red-Nosed Reports.py
+++ 02a-Red-Nosed Reports.py
@@ -5,17 +5,13 @@ for line in f:
     l = line.split()
     l = [int(x) for x in l]
     print(l)
-    if l[0] < l[-1]:
-        for x in range(0,len(l)-1):
-            if (l[x+1] - l[x]) > 3 or (l[x+1] - l[x]) < 1:
-                print(f"{l[x+1]} - {l[x]} = ")
-                illegal +=1
-                break
-    else:
-        for x in range(0,len(l)-1):
-            if (l[x] - l[x+1]) > 3 or (l[x] - l[x+1]) < 1:
-                print(f"{l[x]} - {l[x+1]} = ")
-                illegal +=1
-                break
+    dir = l[0] - l[1]
+    for x in range(len(l)-1):
+        v = l[x] - l[x+1]
+        if abs(v) > 3 or abs(v) < 1 or dir * v < 0:
+            illegal +=1
+
+            break
+        dir += v
 
 print(1000-illegal)
